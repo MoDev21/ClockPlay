6a057bfd7a9b242be83296956d5693ac7190eac4
Moïse Kaseka <e1444815@cmaisonneuve.qc.ca>
2023-09-08 17:32:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,140 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // DOM elements
+    const alarmTimeInput = document.getElementById('alarm-time');
+    const alarmNameInput = document.getElementById('alarm-name-input');
+    const setAlarmButton = document.getElementById('set-alarm');
+    const btn_repeatDate = document.querySelectorAll('.repeat-days > li');
+    const mainClock = document.querySelector('.main-clock');
+
+    const dayText = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    const monthText = ['January','February','March','April','May','June','July','August','September','October','November','December',];
+    var alarmArray;
+
+
+    $.ajax({
+        url: "json/alarms.json",
+        data: "data",
+        dataType: "json",
+        success: function (response) {
+            alarmArray = response;
+            console.log(alarmArray);
+        }
+    });
+
+
+
+    function showTime() { 
+        const today = new Date();
+        let hours = today.getHours();
+        let minutes = today.getMinutes();
+        let seconds = today.getSeconds();
+        hours = checkTime(hours);
+        minutes = checkTime(minutes);
+        seconds = checkTime(seconds);
+        mainClock.innerHTML =  `${hours}:${minutes}:${seconds}`;
+        setTimeout(showTime, 1000)
+    }
+
+    function checkTime(i) { 
+        if (i < 10) { i = "0" + i};
+        return i;
+    }
+    
+
+    showTime()
+
+    // Event listener for the "Set Alarm" button
+    setAlarmButton.addEventListener('click', function () {
+        // Get the selected alarm time from the input field
+        const alarmTime = alarmTimeInput.value;
+        const alarmName = alarmNameInput.value;
+
+        if (alarmTime) {
+            // If a valid time is selected, call the setAlarm function
+            setAlarm(alarmTime, alarmName);
+            console.log(alarmTime, alarmName);
+        } else {
+            // Show an alert if no valid time is selected
+            alert('Please select a valid alarm time.');
+        }
+    });
+
+    btn_repeatDate.forEach(e => e.addEventListener('click', function () {
+            // Get the selected alarm time from the input field
+            console.log(e.textContent);
+        })
+
+    );
+
+    repeatDateSwitcher
+
+    // Function to set the alarm
+    function setAlarm(alarmTime ,alarmNameInput) {
+        let alarmTItle = document.createElement('div');
+        let setClockTime = document.createElement('div');
+        let alarmDates = document.createElement('div');
+        let alarmTimeHolder = document.createElement('div');
+        
+       
+
+        // Get the current time
+        const now = new Date();
+
+        // Combine the current date and selected alarm time to create a Date object
+        const alarmDate = new Date(now.toDateString() + ' ' + alarmTime);
+
+        // Get the current time in milliseconds
+        const currentTime = now.getTime();
+
+        // If the alarm time is earlier than the current time, set it for the next day
+        if (alarmDate.getTime() <= currentTime) {
+            alarmDate.setDate(alarmDate.getDate() + 1);
+        }
+
+        // Calculate the time remaining until the alarm in milliseconds
+        const timeUntilAlarm = alarmDate.getTime() - currentTime;
+
+        // Set a timeout to trigger the alarm when the time is up
+        setTimeout(function () {
+            alert('Time to wake up!');
+        }, timeUntilAlarm);
+
+
+
+        alarmTItle.innerHTML = alarmNameInput;
+        alarmTItle.classList.add('alarm-title');
+        
+
+        alarmTimeHolder.innerHTML = `${checkTime(alarmDate.getHours())}:${checkTime(alarmDate.getMinutes())}`;
+        alarmTimeHolder.classList.add('alarmTimeHolder');
+
+        setClockTime.classList.add('set-alarm');
+        
+
+        // alarmDates.innerHTML = `${alarmDate.getDay()}/${alarmDate.getMonth()}/${alarmDate.getFullYear()}`;
+        for (let index = 1; index <= 2; index++) {
+            alarmDates.innerHTML += `<p> Days <p>`;
+            
+        }
+        
+        alarmDates.classList.add('alarm-dates');
+
+  
+        document.querySelector('.sidebar').appendChild(setClockTime);
+
+        setClockTime.appendChild(alarmTimeHolder);
+        setClockTime.appendChild(alarmTItle);
+        setClockTime.appendChild(alarmDates);
+        
+        let alarmObject = { time:alarmTimeHolder.textContent, title:alarmTItle.textContent, date:alarmDates.textContent}
+
+
+        alarmArray.push(alarmObject);
+        console.log(alarmArray);
+
+
+    }
+});
